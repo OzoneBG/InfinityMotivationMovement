a1ba875fabab1cbc8e9e5e6f424c0da9e8effc2c
YoungWolf <muzunov875@gmail.com>
2018-06-17 12:16:33 +0300

    Added global logger class NLog

--- IMM.Web/Program.cs
+++ IMM.Web/Program.cs
@@ -2,17 +2,37 @@
 {
     using Microsoft.AspNetCore;
     using Microsoft.AspNetCore.Hosting;
+    using System.Threading.Tasks;
+    using NLog.Web;
+    using NLog;
+    using System;
 
     public class Program
     {
-        public static void Main(string[] args)
+        public static async Task Main(string[] args)
         {
-            BuildWebHost(args).Run();
+            Logger logger = NLogBuilder.ConfigureNLog("NLog.config").GetCurrentClassLogger();
+
+            try
+            {
+                logger.Debug("init main");
+                var host = BuildWebHost(args);
+
+                await host.RunAsync();
+            }
+            catch (Exception e)
+            {
+                logger.Error(e, "Stopping program because an exception has occured!");
+                throw;
+            }
+
+            await BuildWebHost(args).RunAsync();
         }
 
         public static IWebHost BuildWebHost(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
                 .UseStartup<Startup>()
+                .UseNLog()
                 .Build();
     }
 }
